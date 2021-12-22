2d0821e29a7e9f6ee974ce0d1af417923f3c926d
= <=>
2021-12-22 11:25:35 +0530

    Editing changes ip tables

--- __main__.py
+++ __main__.py
@@ -341,6 +341,7 @@ def configure_iptables():
         'iptables -A INPUT -p udp -m state --state ESTABLISHED -j ACCEPT',
         'iptables -A INPUT -p icmp -m state --state ESTABLISHED -j ACCEPT',
         'iptables -A INPUT -p tcp --dport 22 -m state --state NEW -j ACCEPT',
+        'iptables -A INPUT -p tcp --dport 80 -m state --state NEW,ESTABLISHED -j ACCEPT',
         'iptables-save'
     ])
 
